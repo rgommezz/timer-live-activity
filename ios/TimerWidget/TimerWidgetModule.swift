17bd2d7d8385d4f9799b10d8566f8681547b78b8
rgommezz <rauliyohmc@gmail.com>
2024-01-12 17:57:06 +0100

    chore: update live activity every second

--- ios/TimerWidget/TimerWidgetModule.swift
+++ ios/TimerWidget/TimerWidgetModule.swift
@@ -11,36 +11,64 @@ import ActivityKit
 
 @objc(TimerWidgetModule)
 class TimerWidgetModule: NSObject {
-
+  private var elapsedTime: TimeInterval = 0
+  private var timer: Timer?
+  private var currentActivity: Activity<TimerWidgetAttributes>?
+  
   private func areActivitiesEnabled() -> Bool {
     return ActivityAuthorizationInfo().areActivitiesEnabled
   }
-
+  
+  private func startTimer() {
+    DispatchQueue.main.async {
+      self.timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { [weak self] _ in
+        guard let strongSelf = self else { return }
+        strongSelf.elapsedTime += 1
+        // Update Live Activity with new elapsedTime
+        let contentState = TimerWidgetAttributes.ContentState(elapsedTimeInSeconds: Int(strongSelf.elapsedTime))
+        Task {
+          await strongSelf.currentActivity?.update(
+            ActivityContent<TimerWidgetAttributes.ContentState>(
+              state: contentState,
+              staleDate: nil
+            )
+          )
+        }
+      }
+    }
+  }
+  
+  private func resetValues() {
+    timer?.invalidate()
+    timer = nil
+    elapsedTime = 0
+    currentActivity = nil
+  }
+  
   @objc
   func startLiveActivity() -> Void {
     if (!areActivitiesEnabled()) {
       // User disabled Live Activities for the app, nothing to do
       return
     }
-
+    
     // Preparing data for the Live Activity
     let activityAttributes = TimerWidgetAttributes()
-    // This will be dynamic later ;)
     let contentState = TimerWidgetAttributes.ContentState(elapsedTimeInSeconds: 0)
     let activityContent = ActivityContent(state: contentState,  staleDate: nil)
-
+    
     do {
       // Request to start a new Live Activity with the content defined above
-      try Activity.request(attributes: activityAttributes, content: activityContent)
+      currentActivity = try Activity.request(attributes: activityAttributes, content: activityContent)
+      startTimer()
     } catch {
       // Handle errors, skipped for simplicity
     }
   }
-
+  
   @objc
   func stopLiveActivity() -> Void {
-    // A task is a unit of work that can run concurrently in a lightweight thread, managed by the Swift runtime
-    // It helps to avoid blocking the main thread
+    resetValues()
     Task {
       for activity in Activity<TimerWidgetAttributes>.activities {
         await activity.end(nil, dismissalPolicy: .immediate)
