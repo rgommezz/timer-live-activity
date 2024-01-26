88441bbddcfc40e46282c608c98e3ccdf859f2c2
rgommezz <rauliyohmc@gmail.com>
2024-01-26 13:49:28 +0100

    chore: new implementation

--- ios/TimerWidget/TimerWidgetLiveActivity.swift
+++ ios/TimerWidget/TimerWidgetLiveActivity.swift
@@ -11,13 +11,13 @@ import SwiftUI
 
 struct TimerWidgetAttributes: ActivityAttributes {
   public struct ContentState: Codable, Hashable {
-    // Dynamic stateful properties about your activity go here!
-    var elapsedTimeInSeconds: Int
+    var startedAt: Date?
     
-    func formattedTime() -> String {
-      let minutes = (elapsedTimeInSeconds % 3600) / 60
-      let seconds = elapsedTimeInSeconds % 60
-      return String(format: "%02d:%02d", minutes, seconds)
+    func getTimeIntervalSinceNow() -> Double {
+      guard let startedAt = self.startedAt else {
+        return 0
+      }
+      return startedAt.timeIntervalSince1970 - Date().timeIntervalSince1970
     }
   }
 }
@@ -27,9 +27,13 @@ struct TimerWidgetLiveActivity: Widget {
     ActivityConfiguration(for: TimerWidgetAttributes.self) { context in
       // Lock screen/banner UI goes here
       VStack {
-        Text(context.state.formattedTime())
-          .font(.title)
-          .fontWeight(.medium)
+        Text(
+          Date(timeIntervalSinceNow: context.state.getTimeIntervalSinceNow()),
+          style: .timer
+        )
+        .font(.title)
+        .fontWeight(.medium)
+        .monospacedDigit()
       }
       .activityBackgroundTint(Color.cyan)
       .activitySystemActionForegroundColor(Color.black)
@@ -37,22 +41,31 @@ struct TimerWidgetLiveActivity: Widget {
       DynamicIsland {
         // Expanded Region
         DynamicIslandExpandedRegion(.center) {
-          Text(context.state.formattedTime())
-            .font(.title)
-            .foregroundColor(.cyan)
-            .fontWeight(.medium)
+          Text(
+            Date(timeIntervalSinceNow: context.state.getTimeIntervalSinceNow()),
+            style: .timer
+          )
+          .font(.title)
+          .foregroundColor(.cyan)
+          .fontWeight(.medium)
+          .monospacedDigit()
         }
       } compactLeading: {
-        Image("TimerIcon")
-          .resizable()
-          .frame(width: 24, height: 24)
-      } compactTrailing: {
-        Text(context.state.formattedTime())
+        Image(systemName: "timer")
+          .imageScale(.medium)
           .foregroundColor(.cyan)
+      } compactTrailing: {
+        Text(
+          Date(timeIntervalSinceNow: context.state.getTimeIntervalSinceNow()),
+          style: .timer
+        )
+        .foregroundColor(.cyan)
+        .frame(maxWidth: 32)
+        .monospacedDigit()
       } minimal: {
-        Image("TimerIcon")
-          .resizable()
-          .frame(width: 24, height: 24)
+        Image(systemName: "timer")
+          .imageScale(.medium)
+          .foregroundColor(.cyan)
       }
       .widgetURL(URL(string: "http://www.apple.com"))
       .keylineTint(Color.red)
@@ -67,18 +80,13 @@ extension TimerWidgetAttributes {
 }
 
 extension TimerWidgetAttributes.ContentState {
-  fileprivate static var long: TimerWidgetAttributes.ContentState {
-    TimerWidgetAttributes.ContentState(elapsedTimeInSeconds: 1846)
-  }
-  
-  fileprivate static var short: TimerWidgetAttributes.ContentState {
-    TimerWidgetAttributes.ContentState(elapsedTimeInSeconds: 30)
+  fileprivate static var initState: TimerWidgetAttributes.ContentState {
+    TimerWidgetAttributes.ContentState(startedAt: Date())
   }
 }
 
 #Preview("Notification", as: .content, using: TimerWidgetAttributes.preview) {
   TimerWidgetLiveActivity()
 } contentStates: {
-  TimerWidgetAttributes.ContentState.short
-  TimerWidgetAttributes.ContentState.long
+  TimerWidgetAttributes.ContentState.initState
 }
