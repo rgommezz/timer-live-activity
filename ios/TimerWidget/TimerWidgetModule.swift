b30f899c8372864905412cf5dc8663fca1d5a011
rgommezz <rauliyohmc@gmail.com>
2024-01-26 14:02:37 +0100

    refactor: implementation

--- ios/TimerWidget/TimerWidgetModule.swift
+++ ios/TimerWidget/TimerWidgetModule.swift
@@ -6,17 +6,41 @@
 //
 
 import Foundation
+import ActivityKit
 
 @objc(TimerWidgetModule)
 class TimerWidgetModule: NSObject {
-  
+
+  private func areActivitiesEnabled() -> Bool {
+    return ActivityAuthorizationInfo().areActivitiesEnabled
+  }
+
   @objc
   func startLiveActivity() -> Void {
-    // TODO implementation
+    if (!areActivitiesEnabled()) {
+      // User disabled Live Activities for the app, nothing to do
+      return
+    }
+    // Preparing data for the Live Activity
+    let activityAttributes = TimerWidgetAttributes()
+    let contentState = TimerWidgetAttributes.ContentState(startedAt: Date())
+    let activityContent = ActivityContent(state: contentState,  staleDate: nil)
+    do {
+      // Request to start a new Live Activity with the content defined above
+      try Activity.request(attributes: activityAttributes, content: activityContent)
+    } catch {
+      // Handle errors, skipped for simplicity
+    }
   }
-  
+
   @objc
   func stopLiveActivity() -> Void {
-    // TODO implementation
+    // A task is a unit of work that can run concurrently in a lightweight thread, managed by the Swift runtime
+    // It helps to avoid blocking the main thread
+    Task {
+      for activity in Activity<TimerWidgetAttributes>.activities {
+        await activity.end(nil, dismissalPolicy: .immediate)
+      }
+    }
   }
 }
