e864f1175aa530afaddfc7cfab45fc67641ba4c7
rgommezz <rauliyohmc@gmail.com>
2024-01-26 19:10:57 +0100

    refactor: implementation

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {Button, SafeAreaView, Text, View} from 'react-native';
+import useTimer from './useTimer.ts';
+
+function App(): React.JSX.Element {
+  const {value, reset, play} = useTimer();
+  return (
+    <SafeAreaView
+      style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <View style={{paddingVertical: 32}}>
+        <Text style={{fontSize: 80, fontVariant: ['tabular-nums']}}>
+          {value}
+        </Text>
+      </View>
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          paddingHorizontal: 48,
+        }}>
+        <View style={{marginRight: 32}}>
+          <Button title="Start" onPress={play} />
+        </View>
+        <Button title="Stop" onPress={reset} />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default App;
